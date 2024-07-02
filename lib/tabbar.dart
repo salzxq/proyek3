376dd659167c9853eba688e9653684c7891f7214
Salam <ogog939@gmail.com>
2024-07-02 22:14:20 +0700

    just login sementara

--- lib/tabbar.dart
+++ lib/tabbar.dart
@@ -1,52 +1,52 @@
-import 'package:flutter/material.dart';
-import 'package:proyek3/login_page.dart';
-import 'order_summary.dart';  // Import the OrderSummaryPage
-import 'favorite.dart';  // Import the favorite.dart file
-import 'notifikasi_order.dart';
-import 'profile.dart';
-import 'main.dart';
-import 'login_page.dart';
+// import 'package:flutter/material.dart';
+// import 'package:proyek3/login_page.dart';
+// import 'order_summary.dart';  // Import the OrderSummaryPage
+// import 'favorite.dart';  // Import the favorite.dart file
+// import 'notifikasi_order.dart';
+// import 'profile.dart';
+// import 'main.dart';
+// import 'login_page.dart';
 
 
-class NavigationMenu extends StatefulWidget {
-  const NavigationMenu({super.key});
+// class NavigationMenu extends StatefulWidget {
+//   const NavigationMenu({super.key});
 
-  @override
-  _NavigationMenuState createState() => _NavigationMenuState();
-}
+//   @override
+//   _NavigationMenuState createState() => _NavigationMenuState();
+// }
 
-class _NavigationMenuState extends State<NavigationMenu> {
-  int _currentIndex = 0;
+// class _NavigationMenuState extends State<NavigationMenu> {
+//   int _currentIndex = 0;
 
-  final List<Widget> _pages = [
-    LoginScreen(),
-    HomeScreen(),
-    OrderSummaryPage(),  // Include OrderSummaryPage
-    Favorite(),
-    notifikasi_order(),
-    Profile(),
-  ];
+//   final List<Widget> _pages = [
+//     LoginScreen(),
+//     HomeScreen(),
+//     OrderSummaryPage(),  // Include OrderSummaryPage
+//     Favorite(),
+//     notifikasi_order(),
+//     Profile(),
+//   ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_sharp), label: 'Keranjang'),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorite'),
-          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notifikasi'),
-          BottomNavigationBarItem(icon: Icon(Icons.account_box), label: 'Profile'),
-        ],
-      ),
-      body: _pages[_currentIndex],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       bottomNavigationBar: BottomNavigationBar(
+//         type: BottomNavigationBarType.fixed,
+//         currentIndex: _currentIndex,
+//         onTap: (index) {
+//           setState(() {
+//             _currentIndex = index;
+//           });
+//         },
+//         items: const [
+//           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+//           BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_sharp), label: 'Keranjang'),
+//           BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorite'),
+//           BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notifikasi'),
+//           BottomNavigationBarItem(icon: Icon(Icons.account_box), label: 'Profile'),
+//         ],
+//       ),
+//       body: _pages[_currentIndex],
+//     );
+//   }
+// }
